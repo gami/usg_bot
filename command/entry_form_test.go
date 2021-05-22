aeee3e1e3fe60234622883341fa1c9993d8cc057
gami <takegamm@gmail.com>
2021-05-22 23:27:45 +0900

    wip

--- command/entry_form_test.go
+++ command/entry_form_test.go
@@ -16,11 +16,18 @@ func TestEntryForm_Resolve(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "OK",
+			c:    &command.EntryForm{},
+			args: args{
+				m: "",
+			},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := &command.EntryForm{}
+			c := tt.c
 			if got := c.Resolve(tt.args.m); got != tt.want {
 				t.Errorf("EntryForm.Resolve() = %v, want %v", got, tt.want)
 			}
